4182515079eb26ce5e59d43d05e92bb9ec5bdc26
lucagcalio <donlucadj@gmail.com>
2025-09-19 15:46:36 +0200

    Warehouse update

--- src/main/java/pharma/Handler/Table/LottoStorageSearchTableView.java
+++ src/main/java/pharma/Handler/Table/LottoStorageSearchTableView.java
@@ -2,15 +2,16 @@ package pharma.Handler.Table;
 
 import algo.ShelvesAssigment;
 import javafx.scene.control.TableView;
+import pharma.Model.FieldData;
 import pharma.config.TableUtility;
 
-public class LottoStorageSearchTableView  extends  TableBase<ShelvesAssigment> {
+public class LottoStorageSearchTableView  extends  TableBase<FieldData> {
     public LottoStorageSearchTableView(String content) {
         super(content);
     }
 
     @Override
-    protected void setupBaseColumns(TableView<ShelvesAssigment> tableView) {
+    protected void setupBaseColumns(TableView<FieldData> tableView) {
         tableView.getColumns().
                 addAll(TableUtility.generate_column_double("Farmaco","nome"),
                         TableUtility.generate_column_double("Tipologia","Tipologia"),
